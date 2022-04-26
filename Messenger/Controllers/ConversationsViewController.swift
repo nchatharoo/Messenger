708bf6e2f399f2a343ecf986aa1575265a24d4c7
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-04-26 16:18:26 +0200

    Setup Login and Registration VC

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -11,7 +11,20 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let loggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !loggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
     }
 }
 
