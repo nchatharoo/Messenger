3bb42b0db363abe3a3b3d22b7dc05e5065192326
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-05-06 16:52:57 +0200

    Move struc to its own file

--- Messenger/Model/ChatAppUser.swift
+++ Messenger/Model/ChatAppUser.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
+    var profilePictureFileName: String {
+        return "\(safeEmail)_profile_picture.png"
+    }
+}
